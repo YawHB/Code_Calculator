1d05b3de6f3f732f52b1a82c3670a7ba80473478
YawHB <yawhboateng@gmail.com>
2023-03-19 17:25:15 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function addNumbers(num1, num2) {
+  const addResult = num1 + num2;
+  return addResult;
+}
+function substractNumbers(num1, num2) {
+  const addResult = num1 - num2;
+  return addResult;
+}
+function multiplyNumbers(num1, num2) {
+  const addResult = num1 * num2;
+  return addResult;
+}
+function divideNumbers(num1, num2) {
+  const addResult = num1 / num2;
+  return addResult;
+}
+
+console.log(addNumbers(2, 5));
+console.log(substractNumbers(2, 5));
+console.log(multiplyNumbers(2, 5));
+console.log(divideNumbers(2, 5));
